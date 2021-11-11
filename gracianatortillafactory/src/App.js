810aa68032973eb081e6bda48d4471d0f1d747c4
Alex Kevakian <akeva001@ucr.edu>
2021-11-11 13:01:12 -0800

    Added home logo

--- gracianatortillafactory/src/App.js
+++ gracianatortillafactory/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <img src={"/images/logo.png"} alt="Logo" />
+    </div>
+  );
 };
 
 export default App;
